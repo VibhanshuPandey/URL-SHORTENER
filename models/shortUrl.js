6de8fb18fa0482315cfcad7b90147176cdf8193e
VibhanshuPandey <pandeyvibhanshu007@gmail.com>
2024-05-01 14:05:56 +0530

    shorturl module implemented

--- models/shortUrl.js
+++ models/shortUrl.js
@@ -1,13 +1,22 @@
 const mongoose = require('mongoose')
+const shortId = require('shortid')
 
 const shortUrlSchema  = new mongoose.Schema({
     full: {
-        String,
+        type: String,
         required: true
     },
     short: {
         type: String,
-        required: true
+        required: true,
+        default: shortId.generate
+    },
+    clicks: {
+        type: Number,
+        required: true,
+        default: 0
     }
 
 })
+
+module.exports = mongoose.model('ShortUrl', shortUrlSchema)
